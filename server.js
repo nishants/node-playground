5a5b9f17da3e143669a7e36cdd20d70906bd0c8e
nishant <nishant.singh87@gmail.com>
2018-03-07 18:30:01 +0530

    add a REST api

--- server.js
+++ server.js
@@ -7,7 +7,20 @@ const Todo = require('./models/todo');
 
 const app = express();
 
-app.use(bodyParser);
+app.use(bodyParser.json());
+
 app.listen(3000, ()=> {
 	console.log("running on 3000");
-})
+});
+
+app.post("/todos", (request, response)=> {
+	const params = request.body.todo;
+	console.log(params)
+	new Todo({
+		text: params.text
+	}).save().then((created)=> {
+		response.send({status: "success", created});
+	}, (error)=> {
+		response.status(400).send({status: "error", error});
+	})
+});
