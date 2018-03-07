9ccf01be0ce68cb55ad3f8d00a44bbfcafdb7f05
nishant <nishant.singh87@gmail.com>
2018-03-07 19:13:50 +0530

    test db config

--- db.js
+++ db.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
-mongoose.connect("mongodb://localhost:27017/todo-app");
+const dbName = process.argv.indexOf('--test') > 0 ? 'test-todo-app' : 'todo-app'
+const dbURL  = 'mongodb://localhost:27017'
+
+mongoose.connect(`${dbURL}/${dbName}`);
 
 module.exports = {mongoose};
