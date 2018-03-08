02d9b095930a6d0c10b1e0a772c857e31fd82c30
nishant <nishant.singh87@gmail.com>
2018-03-08 17:41:06 +0530

    Create test env

--- app/db.js
+++ app/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
-const dbName = process.argv.indexOf('--test') > 0 ? 'test-todo-app' : 'todo-app';
+//const dbName = process.argv.indexOf('--test') > 0 ? 'test-todo-app' : 'todo-app';
 //const dbURL  = process.env.MONGODB_URI || `mongodb://localhost:27017/${dbName}`;
 const dbURL  = process.env.MONGODB_URI || process.env.MONGO_HOST;
 
