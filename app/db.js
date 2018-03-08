366e372922023d9806d6ff92ffbebcdb0382c229
nishant <nishant.singh87@gmail.com>
2018-03-08 18:11:54 +0530

    rename mongo db uri env variable.

--- app/db.js
+++ app/db.js
@@ -1,10 +1,6 @@
 const mongoose = require('mongoose');
+
 mongoose.Promise = global.Promise;
-
-//const dbName = process.argv.indexOf('--test') > 0 ? 'test-todo-app' : 'todo-app';
-//const dbURL  = process.env.MONGODB_URI || `mongodb://localhost:27017/${dbName}`;
-const dbURL  = process.env.MONGODB_URI || process.env.MONGO_HOST;
-
-mongoose.connect(dbURL);
+mongoose.connect(process.env.MONGODB_URI);
 
 module.exports = {mongoose};
